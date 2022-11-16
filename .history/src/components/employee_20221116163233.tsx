7a4f063a407a052163ad823476fb3337db6ed91e
lakindulk <lakindulk9@gmail.com>
2022-11-16 17:01:48 +0530

    three roles ui added

---
+++ .history/src/components/employee_20221116163233.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ButtonAppBar from './ButtonAppBar'
+
+export default function Employee() {
+  return (
+    <div>
+        <ButtonAppBar></ButtonAppBar>
+        <div style={{paddingRight:'150vh',paddingTop:'3vh'}}>
+        <h1>Logged As Employee </h1>
+        </div>
+    </div>
+  )
+}
---
+++ .history/src/components/managerUI_20221116163331.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ButtonAppBar from './ButtonAppBar'
+import { Card, Button, Row, Col, Form } from 'react-bootstrap';
+
+export default function ManagerUI() {
+    return (
+        <div>
+            <ButtonAppBar></ButtonAppBar>
+            <div style={{paddingRight:'150vh',paddingTop:'3vh'}}>
+            <h1>Logged As Manager </h1>
+            </div>
+        </div>
+    )
+}
